dc516dd01ed95a4d963f78618c3188909659e3af
SicEtNon <dante.contact@proton.me>
2024-03-24 15:51:58 -0300

    refactor: add remaps

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -26,9 +26,9 @@ telescope.setup({
 -- load_extension, somewhere after setup function:
 telescope.load_extension("ui-select")
 
-vim.keymap.set("n", "<leader>fi", builtin.find_files, {})
-vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>gs", function()
+vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
+vim.keymap.set("n", "<leader>sl", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>sg", function()
 	builtin.grep_string({ search = vim.fn.input("grep > ") })
 end)
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
